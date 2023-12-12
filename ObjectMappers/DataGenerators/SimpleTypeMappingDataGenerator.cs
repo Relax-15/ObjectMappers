8d5f2ecdd716965cc368fec892f3ffba48c9de3c
relax-15 <relaxkgamane@gmail.com>
2023-12-12 15:43:01 +0200

    Added data generators

---
+++ ObjectMappers/DataGenerators/SimpleTypeMappingDataGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using ObjectMappers.SimpleTypeMapping;
+
+namespace ObjectMappers.DataGenerators;
+
+public class SimpleTypeMappingDataGenerator
+{
+    public static List<User> GetSources(int count = 100)
+    {
+        var faker = new Faker<User>()
+            .Rules((f, o) =>
+            {
+                o.Id = f.Random.Number();
+                o.Name = f.Name.FullName();
+                o.Email = f.Person.Email;
+                o.IsActive = f.Random.Bool();
+                o.DateCreated = DateTime.Now;
+            });
+        return faker.Generate(count);
+    }
+}
